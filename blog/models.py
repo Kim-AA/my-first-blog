71c98e9489d976866afac6f540b7ccf129fe91c4
Kim--AA <aliaga.aguirre.kimberly@gmail.com>
2022-06-11 16:14:07 +0200

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+
+    #atributes
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tittle = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    #methods
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def _str_(self):
+        return self.tittle
+
+
